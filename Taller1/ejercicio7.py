006920872bac64bf60fb9310b9c8fb2f384fae51
Juangalvez1 <juanlopezjosegalvez@gmail.com>
2026-02-14 16:17:44 -0500

    Finalizing exercice 7

--- Taller1/ejercicio7.py
+++ Taller1/ejercicio7.py
@@ -1,3 +1,17 @@
-palabra = "abcdefghijklmnopqrstuvwxyz"
+lista = ["oso", "casa", "murciélago", "ventana", "programación","objetos", "listas", "métodos", "utp"]
 
-print(palabra.find('1'))
+char = input("Ingrese eun caracter, para buscarlo en las palabras de la lista: ")
+
+contador_palabra = 0
+
+for i in lista:
+    if i.find(char) != -1:
+        print(f"{i} - la palabra es {'par' if len(i) % 2 == 0 else 'impar'}")
+        contador_palabra += 1
+    else:
+        pass
+
+if contador_palabra == 0:
+    print(f"No habian palabras con la letra {char} en ellas")
+else:
+    pass
